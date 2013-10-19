065d0e881250b08de2d6e303b9de134dc2b98efa
Daniel Urdaneta <mangafever@gmail.com>
2013-10-19 14:14:53 -0430

    creating some php files
    footer, head, menu, set

---
+++ head.php
@@ -0,0 +1,8 @@
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<!--<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/grids-min.css">-->
+	<link rel="stylesheet" href="css/pure-skin-dsudev.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	
+	<!-- Favicon and touch icons -->
+	<link rel="shortcut icon" href="favicon.ico">
---
+++ menu.php
@@ -0,0 +1,21 @@
+	<div class="pure-hidden-desktop pure-hidden-tablet">
+		<div class="pure-skin-dsudev pure-menu pure-menu-open pure-menu-horizontal">
+			<a href="#" class="pure-menu-heading">DSU <i class="icon-android"></i></a> 
+			<a class="pure-skin-dsudev pure-menu-link pure-button pure-button-large pure-button-primary">
+				menu <i class="icon-ellipsis-vertical"></i>
+				</a>
+			
+		</div>
+	</div>
+	
+	<div class="pure-hidden-phone pure-skin-dsudev pure-menu pure-menu-open pure-menu-horizontal">
+    <a href="#" class="pure-menu-heading">DSU <i class="icon-coffee"></i></a>
+	<!--<div class="pure-hidden-desktop"></div>-->
+    <ul >
+    <!--	<li><a href="#" class="pure-menu-heading pure-menu-selected">DSU <i class="icon-coffee"></i></a>
+</li> -->
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Quests</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+	</div>
